5c3db36cf637732918e6119d87a1101098d0bee6
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2021-11-30 18:32:39 +0100

    added logic to about button

--- app/src/main/java/com/example/silentcalls/MainActivity.java
+++ app/src/main/java/com/example/silentcalls/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.silentcalls;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButtons();
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Not yet implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initButtons(){
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
