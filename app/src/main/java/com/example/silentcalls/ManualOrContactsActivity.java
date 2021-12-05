148eb0963e21cba17b399d6cab68e97427f66d9e
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2021-12-05 10:12:03 +0100

    added logic to chooseFromContacts button

--- app/src/main/java/com/example/silentcalls/ManualOrContactsActivity.java
+++ app/src/main/java/com/example/silentcalls/ManualOrContactsActivity.java
@@ -3,12 +3,29 @@ package com.example.silentcalls;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ManualOrContactsActivity extends AppCompatActivity {
+
+    Button chooseFromContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_or_contacts);
+
+        initButtons();
+        chooseFromContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ManualOrContactsActivity.this,"Not yet implemented",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initButtons(){
+        chooseFromContacts = findViewById(R.id.btnChooseFromContacts);
     }
 }
