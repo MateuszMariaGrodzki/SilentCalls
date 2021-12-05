7136bc3a98299a994dc5d6bc1cf94ac17b60c3ef
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2021-12-05 10:22:17 +0100

    added new PhoneCall Activity

---
+++ app/src/main/java/com/example/silentcalls/PhoneCallActivity.java
@@ -0,0 +1,14 @@
+package com.example.silentcalls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_call);
+    }
+}
